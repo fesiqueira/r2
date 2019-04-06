75a20ead3e79c34b6a30d569c364c04fe010639a
Felipe Siqueira Pinheiro <fesiqp@gmail.com>
2019-04-05 23:18:02 -0300

    improved logging

--- examples/basic/basic.ino
+++ examples/basic/basic.ino
@@ -5,16 +5,21 @@ static byte macAddr[] = {0x74, 0x69, 0x69, 0x2D, 0x30, 0x31};
 Server Server(ethercard, macAddr);
 
 void setup() {
-  Serial.begin(57600);
-  Serial.println(F("DHCP Example"));
+    Serial.begin(57600);
+    Serial.println(F("DHCP Example"));
 
-  if (Server.begin() == 0) {
-    Serial.println(F("Failed to access controller"));
-  }
+    if (Server.begin() == 0) {
+        Serial.println(F("Failed to access controller"));
+    } else {
+        Serial.println(F("Controller found. Setting up DHCP..."));
+    }
 
-  if (!Server.DHCP()) {
-    Serial.println(F("DHCP failed"));
-  }
+    if (!Server.DHCP()) {
+        Serial.println(F("DHCP failed"));
+    } else {
+        Serial.println(F("DHCP success"));
+        Server.describe();
+    }
 }
 
 void loop() { Server.handle(); }
